a51af33361b60ca65cca40e5fd50c16bebc653a8
hongmingjie <hongmingjie@espressif.com>
2018-01-08 17:47:10 +0800

    fix:..

--- upload_to_server.py
+++ upload_to_server.py
@@ -17,7 +17,7 @@ class Uploader(object):
     efuse        = ''
     chk_repeat_flg    = True     # True:check mac repeat, False:no check
     po_type        = 0            # 0:normal, 1:rework(fw..), 2:rework(mac)
-    def __init__(self, **params):
+    def __init__(self, params):
         self.server_ip      = params['server_ip']
         self.server_port    = params['server_port']
         self.device_type    = params['device_type']
@@ -29,22 +29,27 @@ class Uploader(object):
         self.factory_sid    = params['factory_sid']
         self.batch_sid      = params['batch_sid']
         self.efuse          = params['efuse']
-        self.chk_repeat_flg = params['chk_repeat_flg']
-        self.po_type        = params['po_type']
+        if params['chk_repeat_flg'].lower()=='true':
+            self.chk_repeat_flg = True
+        else:
+            self.chk_repeat_flg = False
+        self.po_type        = int(params['po_type'])
 
     def client(self):
         return True
 
 def main():
-    try:
-        # import ConfigParser
-        # config = ConfigParser.ConfigParser()
-        # config = read(u'config/setting.ini')
-        params = json.loads(sys.args[1])
-        uploader = Uploader(params)
-    except:
-        print('read params ERROR')
-        return
+
+    # import ConfigParser
+    # config = ConfigParser.ConfigParser()
+    # config = read(u'config/setting.ini')
+    params = sys.argv[1]
+    print params
+    params = json.loads(params)
+    uploader = Uploader(params)
+
+    print('read params ERROR')
+    #return
 
     try:
         if uploader.client():
@@ -59,3 +64,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
+"""
+"{\"server_ip\":\"192.168.1.1\", \"server_port\":\"6666\", \"device_type\":\"WROOM02\", \"fw_ver\":\"\", \"esp_mac\":\"2c3ae8080000\", \"cus_mac\":\"\", \"flash_id\":\"\", \"test_result\":\"success\", \"factory_sid\":\"esp-fae-test-a95342f3\", \"batch_sid\":\"6a5fbb0d43\", \"efuse\":\"\", \"chk_repeat_flg\":\"True\", \"po_type\":\"0\"}"
+"""
