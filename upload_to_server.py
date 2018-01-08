7e4d0665726dbb1b20871ac7b2f1280f6f1819ad
hongmingjie <hongmingjie@espressif.com>
2018-01-08 19:36:53 +0800

    ver_1.0.0

--- upload_to_server.py
+++ upload_to_server.py
@@ -2,40 +2,70 @@
 import json
 import sys
 import os
+import socket
 
 class Uploader(object):
     server_ip     = ''
     server_port = 0
-    device_type = ''
-    fw_ver        = ''
-    esp_mac        = ''
-    cus_mac        = ''        # custom mac
-    flash_id    = ''
-    test_result    = ''
-    factory_sid    = ''
-    batch_sid     = ''
-    efuse        = ''
-    chk_repeat_flg    = True     # True:check mac repeat, False:no check
-    po_type        = 0            # 0:normal, 1:rework(fw..), 2:rework(mac)
+    HttpJson = {
+    	"path":"",
+    	"method":"",
+    	"testdata":{
+    		"device_type":   "ESP_WROOM02",
+			"fw_ver":        "v1.0.0.0",
+			"esp_mac":       "22:22:22:22:22:10",
+			"cus_mac":       "",
+			"flash_id":      "19191919",
+			"test_result":   "success",
+			"factory_sid":   "esp-fae-test-a95342f3",
+			"batch_sid":     "6a5fbb0d43",
+			"efuse":        "",
+			"chk_repeat_flg": True,
+			"po_type":       0,
+    	}
+    }
+
     def __init__(self, params):
-        self.server_ip      = params['server_ip']
-        self.server_port    = params['server_port']
-        self.device_type    = params['device_type']
-        self.fw_ver         = params['fw_ver']
-        self.esp_mac        = params['esp_mac']
-        self.cus_mac        = params['cus_mac']
-        self.flash_id       = params['flash_id']
-        self.test_result    = params['test_result']
-        self.factory_sid    = params['factory_sid']
-        self.batch_sid      = params['batch_sid']
-        self.efuse          = params['efuse']
+    	self.server_ip      = params['server_ip']
+        self.server_port    = int(params['server_port'])
+
+    	self.HttpJson["path"] = "/testdata"
+    	self.HttpJson["method"] = "POST"
+    	self.HttpJson["testdata"]["device_type"]    = params['device_type']
+        self.HttpJson["testdata"]["fw_ver"]         = params['fw_ver']
+        self.HttpJson["testdata"]["esp_mac"]        = params['esp_mac']
+        self.HttpJson["testdata"]["cus_mac"]        = params['cus_mac']
+        self.HttpJson["testdata"]["flash_id"]       = params['flash_id']
+        self.HttpJson["testdata"]["test_result"]    = params['test_result']
+        self.HttpJson["testdata"]["factory_sid"]    = params['factory_sid']
+        self.HttpJson["testdata"]["batch_sid"]      = params['batch_sid']
+        self.HttpJson["testdata"]["efuse"]          = params['efuse']
         if params['chk_repeat_flg'].lower()=='true':
-            self.chk_repeat_flg = True
+            self.HttpJson["testdata"]["chk_repeat_flg"] = True
         else:
-            self.chk_repeat_flg = False
-        self.po_type        = int(params['po_type'])
+            self.HttpJson["testdata"]["chk_repeat_flg"] = False
+        self.HttpJson["testdata"]["po_type"]        = int(params['po_type'])
+        print self.HttpJson
 
     def client(self):
+    	try:
+    		print (self.server_ip, self.server_port)
+    		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    		s.connect((self.server_ip, self.server_port))
+    	except:
+    		print ("conn to server fail")
+    		return -1
+
+    	send_data_json = json.dumps(self.HttpJson)
+    	send_data_json += '\n'
+    	s.send(send_data_json)
+    	rsp = s.recv(1024)
+    	print rsp
+    	rsp = json.loads(rsp)
+    	if rsp["status"] == 500:
+    		print (rsp["message"])
+
+    	s.close()
         return True
 
 def main():
@@ -44,12 +74,13 @@ def main():
     # config = ConfigParser.ConfigParser()
     # config = read(u'config/setting.ini')
     params = sys.argv[1]
-    print params
+    #print params
     params = json.loads(params)
     uploader = Uploader(params)
 
-    print('read params ERROR')
-    #return
+	# except:
+	#     print('read params ERROR')
+	#     return
 
     try:
         if uploader.client():
@@ -66,5 +97,5 @@ if __name__ == "__main__":
     main()
     
 """
-"{\"server_ip\":\"192.168.1.1\", \"server_port\":\"6666\", \"device_type\":\"WROOM02\", \"fw_ver\":\"\", \"esp_mac\":\"2c3ae8080000\", \"cus_mac\":\"\", \"flash_id\":\"\", \"test_result\":\"success\", \"factory_sid\":\"esp-fae-test-a95342f3\", \"batch_sid\":\"6a5fbb0d43\", \"efuse\":\"\", \"chk_repeat_flg\":\"True\", \"po_type\":\"0\"}"
+"{\"server_ip\":\"120.76.204.21\", \"server_port\":\"6666\", \"device_type\":\"WROOM02\", \"fw_ver\":\"\", \"esp_mac\":\"2c3ae8080000\", \"cus_mac\":\"\", \"flash_id\":\"\", \"test_result\":\"success\", \"factory_sid\":\"esp-fae-test-a95342f3\", \"batch_sid\":\"6a5fbb0d43\", \"efuse\":\"\", \"chk_repeat_flg\":\"True\", \"po_type\":\"0\"}"
 """
